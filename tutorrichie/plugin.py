67fe19acf49c28f8b890c07966e46ecdfffe8c1c
Régis Behmo <regis@behmo.com>
2021-12-20 22:38:41 +0100

    feat: upgrade to maple

--- tutorrichie/plugin.py
+++ tutorrichie/plugin.py
@@ -15,7 +15,7 @@ config = {
     "defaults": {
         "VERSION": __version__,
         "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-richie:{{ RICHIE_VERSION }}",
-        "RELEASE_VERSION": "v2.8.2",
+        "RELEASE_VERSION": "v2.9.1",
         "HOST": "courses.{{ LMS_HOST }}",
         "MYSQL_DATABASE": "richie",
         "MYSQL_USERNAME": "richie",
