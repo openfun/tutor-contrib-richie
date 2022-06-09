2e40bb0015073088eced4233af0bca652c5248b0
Régis Behmo <regis@behmo.com>
2022-06-09 22:31:28 +0200

    v14.0.0: upgrade to nutmeg
    This is a minimal upgrade. We should upgrade the underlying services in the
    near future. We should also upgrade to the v1 plugin API.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_packages(exclude=["tests*", "contrib*"]),
     include_package_data=True,
     python_requires=">=3.5",
-    install_requires=["tutor>=13.0.0,<14.0.0"],
+    install_requires=["tutor>=14.0.0,<15.0.0"],
     entry_points={
         "tutor.plugin.v0": [
             "richie = tutorrichie.plugin"
@@ -51,8 +51,6 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
