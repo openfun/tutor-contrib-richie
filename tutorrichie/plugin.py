d01358a3de2fa05abb787ba6d8bde09037ac9fc5
Sofiane Bebert <s.bebert@diceytech.co.uk>
2022-08-19 08:20:21 +0100

    feat: upgrade richie to v2.15.1

--- tutorrichie/plugin.py
+++ tutorrichie/plugin.py
@@ -15,7 +15,7 @@ config = {
     "defaults": {
         "VERSION": __version__,
         "DOCKER_IMAGE": "{{ DOCKER_REGISTRY }}overhangio/openedx-richie:{{ RICHIE_VERSION }}",
-        "RELEASE_VERSION": "v2.9.1",
+        "RELEASE_VERSION": "v2.15.1",
         "HOST": "courses.{{ LMS_HOST }}",
         "MYSQL_DATABASE": "richie",
         "MYSQL_USERNAME": "richie",
